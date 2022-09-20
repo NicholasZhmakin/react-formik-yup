240abbeaa441b018d4af81910303e136404819ba
NicholasZhmakin <40457361+NicholasZhmakin@users.noreply.github.com>
2022-09-20 11:41:04 +0300

    Add Yup strings schema examples

---
+++ YupSchemaExamples/strings.ts
@@ -0,0 +1,56 @@
+import * as Yup from 'yup';
+
+const Schema1 = Yup.object().shape({
+  email: Yup.string()
+    .email('Must be a valid email')
+    .max(255)
+    .required('Email is required'),
+  firstName: Yup.string().max(255).required('First name is required'),
+  lastName: Yup.string().max(255).required('Last name is required'),
+  password: Yup.string().max(255).required('password is required'),
+  policy: Yup.boolean().oneOf([true], 'This field must be checked'),
+});
+
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+/*
+  string.ensure(): Schema
+  Transforms undefined and null values to an empty string along with setting the default to an empty string.
+*/
+const Schema2 = Yup.object().shape({
+  email: Yup.string()
+    .matches(emailRegex, 'Not valid email')
+    .required('Email is required'),
+  url: Yup.url('Not valid url').ensure(),
+});
+
+/*
+  string.length(limit: number | Ref, message?: string | function): Schema
+  Set a required length for the string value. 
+  The ${length} interpolation can be used in the message argument
+*/
+const Schema3 = Yup.object().shape({
+  username: Yup.string()
+    .length(10, 'Username must contain 10 characters')
+    .required('username is required'),
+});
+
+/*
+  string.trim(message?: string | function): Schema
+  Transforms string values by removing leading and trailing whitespace. If strict() is set it will only validate that the value is trimmed.
+*/
+const Schema4 = Yup.object().shape({
+  username: Yup.string().trim(),
+});
+
+/*
+  string.lowercase(message?: string | function): Schema
+  Transforms the string value to lowercase. If strict() is set it will only validate that the value is lowercase.
+
+  string.uppercase(message?: string | function): Schema
+  Transforms the string value to uppercase. If strict() is set it will only validate that the value is uppercase.
+*/
+const Schema5 = Yup.object().shape({
+  firstName: Yup.string().lowercase().trim().required('First name is required'),
+  lastName: Yup.string().uppercase().trim().required('Last name is required'),
+});
